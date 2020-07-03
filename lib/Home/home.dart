d349449f19a998b90690789f9007b785cc275384
Rizky syawalassana <syawalassana96@gmail.coom>
2020-07-03 20:08:27 +0700

    error display buat transaksi

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,9 +1,20 @@
+import 'package:ayodolan/Transaksi/buatTransaksi.dart';
+import 'package:ayodolan/Wisata/objekwisata.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ayodolan/Login/loginScreen.dart';
+
 void main() {
   runApp(MaterialApp(
     title: "Ayo Dolan Apps",
-    debugShowCheckedModeBanner: false,
+    debugShowCheckedModeBanner: true,
     home: HomePage(),
+    initialRoute: '/',
+    routes: <String, WidgetBuilder>{
+      '/wisata/objekwisata.dart': (context) => Objekwisata(),
+      //'/second': (context) => SecondHome(),
+    },
   ));
 }
 
@@ -12,53 +23,180 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends HomeController {
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              backgroundColor: Colors.white,
-              expandedHeight: 200.0,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Text('Ayo Dolan Apps', style: TextStyle(
-                    color: Colors.black,
-                ), textAlign: TextAlign.left),
-              ),
-            ),
 
-            SliverList(
-              delegate: SliverChildBuilderDelegate([
-                GridView.builder(
-                shrinkWrap: true,
-                  itemBuilder: (BuildContext _, int index) {
-                    return Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
+        body: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                backgroundColor: Colors.white,
+                expandedHeight: 200.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text('Ayo Dolan Apps', style: TextStyle(
+                    color: Colors.black,
+                  ), textAlign: TextAlign.left),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  GridView.count(
+                    shrinkWrap: true,
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 10.0,
+                    children: <Widget>[
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Container(
-                               decoration: BoxDecoration(
+                              decoration: BoxDecoration(
                                 color: Colors.red.withOpacity(.2),
-                                borderRadius: BorderRadius.circular(10.0),
+                                borderRadius: BorderRadius.circular(10.0)
                               ),
-                                 child: Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Icon(Icons.hotel),
-                              )
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child:
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        new MaterialPageRoute(
+                                            builder: (context) => Objekwisata()));
+                                  },
+                                  child: Icon(Icons.beach_access),
+                                  ),
+                                ),
                               ),
-                              Text('Hotel')
-                           ],
-                          )
-                       );
-                    },
-                    itemCount: 8,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      mainAxisSpacing: 20,
-                      crossAxisSpacing: 20
+                            Text('Objek Wisata', textAlign: TextAlign.center),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.red.withOpacity(.2),
+                                  borderRadius: BorderRadius.circular(10.0)
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child:
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        new MaterialPageRoute(
+                                            builder: (context) => Objekwisata()));
+                                  },
+                                  child: Icon(Icons.event_available),
+                                ),
+                              ),
+                            ),
+                            Text('Agenda Event', textAlign: TextAlign.center),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.red.withOpacity(.2),
+                                  borderRadius: BorderRadius.circular(10.0)
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child:
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        new MaterialPageRoute(
+                                            builder: (context) => Objekwisata()));
+                                  },
+                                  child: Icon(Icons.airplanemode_active),
+                                ),
+                              ),
+                            ),
+                            Text('Paket Wisata', textAlign: TextAlign.center),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.red.withOpacity(.2),
+                                  borderRadius: BorderRadius.circular(10.0)
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child:
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        new MaterialPageRoute(
+                                            builder: (context) => registrasi()));
+                                  },
+                                  child: Icon(Icons.monetization_on),
+                                ),
+                              ),
+                            ),
+                            Text('Order', textAlign: TextAlign.center),
+                          ],
+                        ),
+                      ),
+                          ],
+                        ),
+                      ]),
+                      ),
+
+                    ],
+                  ),
+                  );
+  }     
+}
+class MenuIcon extends StatelessWidget {
+  final IconData icon;
+  final Color iconColor;
+  final String labelMenu;
+
+  MenuIcon({
+    this.icon,
+    this.iconColor,
+    this.labelMenu,
+    Null Function() onPressed,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: iconColor.withOpacity(.3),
+                borderRadius: BorderRadius.circular(20.0),
               ),
+              child: Padding(
+                padding: const EdgeInsets.all(6.1),
+                child: Icon(icon, color: iconColor),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(labelMenu)
           ],
         )
     );
