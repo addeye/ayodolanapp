6572937c848a03cff0e1322e3ca6a48b05a0ec7c
Rizky syawalassana <syawalassana96@gmail.coom>
2020-07-23 20:42:51 +0700

    merge sidang

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -6,7 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class CallApi {
 
 
-  final String _url =  'https://3c58207df1f4.ngrok.io/api/';
+  final String _url =  'https://18e40ed566d9.ngrok.io/api/';
 
   postData(data, apiUrl) async {
     var fullUrl = _url + apiUrl;
